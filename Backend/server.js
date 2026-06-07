a259d54e86fe978dd1b69bab5b9e5335fb95d06d
Nikhil <3717nikhil@gmail.com>
2026-06-07 11:25:22 +0530

    refactor server entry point and update package.json for consistent file naming

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,17 @@
 require("dotenv").config();
 
-const app = require("./src/App")
-const connectDB = require("./src/db/db")
-const {createServer} = require("http");
+const app = require("./src/App");
+const connectDB = require("./src/db/db");
+const { createServer } = require("http");
 
-const initSocketServer = require("./src/socket/socket.Server")
+const initSocketServer = require("./src/socket/socket.Server");
 const httpServer = createServer(app);
 connectDB();
 
 initSocketServer(httpServer);
 
-httpServer.listen(3000, () => {
-  console.log("Server is running on port 3000")
-})
+const PORT = process.env.PORT || 3000;
+
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
